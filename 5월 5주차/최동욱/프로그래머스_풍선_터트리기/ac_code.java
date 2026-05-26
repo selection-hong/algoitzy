5a0d15e9c73fbf492abdd6e3d1a4df249878f8ff
DWinging <kong2g20@gmail.com>
2026-05-26 19:26:46 +0900

    Feat : 풍선 터트리기 풀이 추가

---
+++ 5월 5주차/최동욱/프로그래머스_풍선_터트리기/ac_code.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    final int INF = 1_000_000_001;
+    
+    public int solution(int[] arr) {
+        int len = arr.length, top = -1;
+        int[] stack = new int[len + 1];
+        stack[++top] = INF;
+        for(int i = len - 1; i >= 0; i--) {
+            int val = arr[i];
+            if(stack[top] > val) stack[++top] = val;
+        }
+        
+        int answer = 0, left = INF;
+        for(int val : arr) {
+            if(stack[top] == val) {
+                answer++;
+                top--;
+            } else if(val < left) {
+                answer++;
+                if(val < left) left = val;
+            }   
+        }
+        
+        return answer;
+    }
+}
