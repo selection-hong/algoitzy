4697e89663084600211497fffca8dced0f732ed1
DWinging <103321993+DWinging@users.noreply.github.com>
2026-08-18 19:46:31 +0900

    Update Solution.java

--- 8월 3주차/최동욱/프로그래머스_호텔_방_배정/Solution.java
+++ 8월 3주차/최동욱/프로그래머스_호텔_방_배정/Solution.java
@@ -11,10 +11,7 @@ class Solution {
         for(int i = 0; i < n; i++) {
             long room = room_number[i];
             
-            if(!map.isEmpty() && map.containsKey(room)) {
-                room = find(map, num, room);
-            }
-            
+            room = find(map, num, room);
             map.put(room, i);
             answer[i] = room;
             num[i] = room + 1;
@@ -24,7 +21,7 @@ class Solution {
     }
     
     private long find(Map<Long, Integer> map, long[] num, long room) {
-        if(!map.isEmpty() && map.containsKey(room)) {
+        if(map.containsKey(room)) {
             return num[map.get(room)] = find(map, num, num[map.get(room)]);    
         } else {
             return room;
