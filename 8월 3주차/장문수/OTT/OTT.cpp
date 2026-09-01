231c9d0832d1684ccc8d6b2a1025686ca4837bde
Ha-coding-user <gkwnsdn1130@gmail.com>
2026-09-01 16:14:38 +0900

    ms code

---
+++ 8월 3주차/장문수/OTT/OTT.cpp
@@ -0,0 +1,260 @@
+﻿/*
+* 전체 영화가 들어있는 pq 선언 (1개)
+* 장르별 pq 선언 (5개)
+*
+* ※ 마지막 로직
+* pq를 어떻게 구분할 것인가
+* genre_id를 key로 가지는 unordered_map<int, priority_queue<MOVIE_INFO, vector<MOVIE_INFO>, MOVIE_COMP>> pq_map 사용
+* 사용 시 priority_queue<MOVIE_INFO, vector<MOVIE_INFO>, MOVIE_COMP> pq = pq_map[genre_id]로 접근
+*
+*
+*/
+
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <iostream>
+
+#define MAX_MOVIE_ID 1000000001
+
+using namespace std;
+
+struct RESULT
+{
+    int cnt;
+    int IDs[5];
+};
+
+struct MOVIE_INFO {
+    int id, genre_id, total_score;
+    int apply_time, last_update;
+};
+
+struct MOVIE_COMP {
+    bool operator() (const MOVIE_INFO& a, const MOVIE_INFO& b) {
+        if (a.total_score == b.total_score) {
+            return a.apply_time < b.apply_time; // apply_time 내림차순
+        }
+
+        return a.total_score < b.total_score; // total_score 내림차순
+    }
+};
+
+unordered_map<int, priority_queue<MOVIE_INFO, vector<MOVIE_INFO>, MOVIE_COMP>> pq_map;
+
+int movie_idx;
+MOVIE_INFO movies[10000]; // 영화 정보
+
+unordered_map<int, int> movie_map; // mID 기반 movie_idx 조회
+
+unordered_map<int, bool> exist_map; // 영화 존재 여부
+
+unordered_map<long long, int> watch_map; // {uID * MAX_MOVIE_ID + mID} -> 시청 횟수
+
+int watch_movie_count[1001]; // uID_idx -> 시청한 영화 개수
+vector<int> watch_movie[1001]; // uID_idx -> 시청한 영화 번호
+
+
+int apply_time = 0; // 영화 등록 시기
+
+int duple_watch_idx;
+int duple_watch[10001];
+
+void init(int N)
+{
+    pq_map.clear();
+    movie_idx = 0;
+    movie_map.clear();
+    exist_map.clear();
+    watch_map.clear();
+    fill(watch_movie_count, watch_movie_count + 1001, 0);
+    fill(watch_movie, watch_movie + 1001, vector<int>());
+    apply_time = 0;
+
+    return;
+}
+
+/*
+* add 시 전체 영화 및 장르에 맞는 pq에 push
+* ㄴ 이때, 영화가 등록된 시기 apply++
+* 존재하는 영화 표시용 unordered_map<int, bool> exist_map 사용
+* 영화 정보는 {apply_time, total_score, genre_id, last_update}로 구성
+* pq 정렬 조건은 total_scort 내림차순, apply_time 내림차순
+*/
+int add(int mID, int mGenre, int mTotal)
+{
+    // 이미 영화가 있을 경우
+    if (exist_map[mID]) {
+        return 0;
+    }
+
+    movies[movie_idx] = { mID, mGenre, mTotal, apply_time++, 0 }; // 영화 정보 저장
+    pq_map[0].push(movies[movie_idx]); // 전체 영화 pq에 push
+    pq_map[mGenre].push(movies[movie_idx]); // 장르별 pq에 push
+    exist_map[mID] = true; // 영화 존재 표시
+    movie_map[mID] = movie_idx;
+    movie_idx++;
+
+    return 1;
+}
+
+
+/*
+* 영화 존재 여부용 unordered_map<int, bool> exist_map 사용
+* ㄴ exist_map[mID] = false 라면 없는것 -> return 0
+* ㄴ 그 외 exist_map[mID] = false -> return 1
+*/
+int erase(int mID)
+{
+    if (exist_map[mID]) {
+        exist_map[mID] = false;
+        return 1;
+    }
+
+    return 0;
+}
+
+
+/*
+* watch 시 영화 존재 여부 확인
+* ㄴ exist_map[mID] = false 라면 없는것 -> return 0
+* ㄴ watch_map[{uID, mID}] = 1 이상 이라면 이미 시청한것 -> return 0
+* ㄴ 그 외 watch_map[{uID, mID}] = mRating, total_score += mRating, last_update++
+*    ㄴ 이후 해당 장르에 해당하는 pq에 영화 정보 push -> return 1
+*/
+int watch(int uID, int mID, int mRating)
+{
+    // 영화가 있을 경우
+    if (exist_map[mID]) {
+        long long key = (long long)uID * MAX_MOVIE_ID + mID;
+        if (!watch_map[key]) { // 영화 시청 기록이 없을 경우
+            watch_map[key] = mRating; // 시청 기록 저장
+
+            // 영화 정보 업데이트
+            movies[movie_map[mID]].total_score += mRating;
+            movies[movie_map[mID]].last_update++;
+            pq_map[0].push(movies[movie_map[mID]]); // 전체 영화 pq에 push
+            pq_map[movies[movie_map[mID]].genre_id].push(movies[movie_map[mID]]); // 장르별 pq에 push
+
+            // 시청한 영화 목록 업데이트
+            watch_movie_count[uID]++;
+            watch_movie[uID].push_back(mID);
+
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+bool resent_info_check(MOVIE_INFO& a, MOVIE_INFO& b) {
+    return a.last_update == b.last_update;
+}
+
+/*
+* suggest 시 uID가 시청한 영화 목록 확인
+* uID가 시청한 영화를 저장할 vector<int>[1000] 사용 -> int[uID][0 ~ k] = 시청한 영화 번호
+* uID가 시청한 최근 시청한 영화 5개를 표시할 vector<int>[1000] 사용 -> int[uID] = 시청한 영화 개수
+*
+* 시청한 영화가 없다면(int[uID] == 0) 전체 영화 pq에서 top 5개를 res.IDs에 넣으면서 res.cnt++;
+* res.cnt의 초기값은 0
+*
+* 시청한 영화가 있다면 가장 많이 시청한 장르 확인
+* ㄴ for (int[uID] -> int[uID] - 5 && int[uID] > 0 ) -> int[uID][i]
+* ㄴ 총점이 더 높은 장르 확인
+*
+* 해당 장르의 pq에서 top 5개를 res.IDs에 넣으면서 res.cnt++;
+* ㄴ watch_map[{uID, mID}] = 1 이상인 영화는 제외
+* ㄴ res.IDs에 저장하면서 res.cnt++;
+* ㄴ 이후 res.ID에 있는 영화를 다시 pq에 push
+*/
+RESULT suggest(int uID)
+{
+    RESULT res;
+    res.cnt = 0;
+    duple_watch_idx = 0;
+
+    int cnt = watch_movie_count[uID];
+    if (!cnt) { // 시청한 영화가 없을 경우
+        // 전체 영화에서 조회
+        priority_queue<MOVIE_INFO, vector<MOVIE_INFO>, MOVIE_COMP>& pq = pq_map[0];
+        while (res.cnt < 5 && !pq.empty()) {
+            MOVIE_INFO movie_data = pq.top();
+            pq.pop();
+
+            // 존재하지 않는 영화일 경우 continue
+            if (!exist_map[movie_data.id]) {
+                continue;
+            }
+
+            // 최신 영화 정보 여부 확인
+            if (resent_info_check(movies[movie_map[movie_data.id]], movie_data)) {
+                // 시청한 영화인지 확인
+                long long key = (long long)uID * MAX_MOVIE_ID + movie_data.id;
+                if (watch_map[key]) {
+                    duple_watch[duple_watch_idx++] = movie_data.id;
+                    continue;
+                }
+
+                res.IDs[res.cnt++] = movie_data.id;
+            }
+        }
+
+        for (int i = 0; i < res.cnt; i++) {
+            pq.push(movies[movie_map[res.IDs[i]]]);
+        }
+    }
+    else {
+        int genre_id = 0;
+        int max_score = 0;
+        for (int i = cnt - 1; i >= cnt - 5 && i >= 0; i--) {
+            int movie_id = watch_movie[uID][i];
+            if (!exist_map[movie_id]) {
+                cnt--;
+                continue;
+            }
+
+            long long key = (long long)uID * MAX_MOVIE_ID + movie_id;
+            if (max_score < watch_map[key]) {
+                max_score = watch_map[key];
+                genre_id = movies[movie_map[movie_id]].genre_id;
+            }
+        }
+
+        // 특정 장르의 영화만 추천
+        priority_queue<MOVIE_INFO, vector<MOVIE_INFO>, MOVIE_COMP>& pq = pq_map[genre_id];
+        while (res.cnt < 5 && !pq.empty()) {
+            MOVIE_INFO movie_data = pq.top();
+            pq.pop();
+
+            // 존재하지 않는 영화일 경우 continue
+            if (!exist_map[movie_data.id]) {
+                continue;
+            }
+
+            // 최신 영화 정보 여부 확인
+            if (resent_info_check(movies[movie_map[movie_data.id]], movie_data)) {
+                // 시청한 영화인지 확인
+                long long key = (long long)uID * MAX_MOVIE_ID + movie_data.id;
+                if (watch_map[key]) {
+                    duple_watch[duple_watch_idx++] = movie_data.id;
+                    continue;
+                }
+
+                res.IDs[res.cnt++] = movie_data.id;
+            }
+        }
+
+        for (int i = 0; i < res.cnt; i++) {
+            pq.push(movies[movie_map[res.IDs[i]]]);
+        }
+
+        for (int i = 0; i < duple_watch_idx; i++) {
+            pq.push(movies[movie_map[duple_watch[i]]]);
+        }
+    }
+
+    return res;
+}
---
+++ 8월 3주차/장문수/던전 탈출/던전탈출.cpp
@@ -0,0 +1,349 @@
+﻿#include <iostream>
+#include <queue>
+#include <cmath>
+#include <vector>
+#include <cstring>
+#include <unordered_map>
+
+#define MAP_SIZE_MAX	350
+
+using namespace std;
+
+// 맵 정보 입력
+int n, max_stamina;
+int* map[MAP_SIZE_MAX];
+
+// [gate_id] = gate 좌표 (y * n + x)
+unordered_map<int, int> gate_map;
+
+// 추가된 게이트 정보
+int add_cnt;
+int add_gate[200];
+
+// 4방향 이동
+int dx[] = { 1, 0, -1, 0 };
+int dy[] = { 0, 1, 0, -1 };
+
+// 게이트 간 이동 시간 정보 구조체
+struct NODE_INFO {
+	int gate_id, time;
+};
+
+// 게이트 간 이동 시간 정보
+vector<NODE_INFO> gate_graph[202];
+// 게이트간 연결 여부
+bool gate_edge[202][202];
+
+// bfs 용 게이트 방문 정보
+NODE_INFO visit[MAP_SIZE_MAX][MAP_SIZE_MAX];
+
+// bfs 용 큐 구조체
+struct BFS_INFO {
+	int y, x, time;
+};
+
+// bfs
+int front, rear;
+BFS_INFO que[MAP_SIZE_MAX * MAP_SIZE_MAX];
+
+// dijkstra 용 우선순위 큐 비교 구조체 
+struct Comp {
+	bool operator() (NODE_INFO& a, NODE_INFO& b) {
+		return a.time > b.time;
+	}
+};
+
+
+// dijkstra 용 게이트 방문 정보 구조체
+struct DATA {
+	int call_cnt, time;
+};
+
+// dijkstra 용 게이트 방문 정보
+DATA dijk_visit[202];
+
+// dijkstra 용 방문 배열 재사용을 위한 호출 횟수
+int call_cnt;
+
+
+/* 초기화 함수 */
+void init(int N, int mMaxStamina, int mMap[MAP_SIZE_MAX][MAP_SIZE_MAX])
+{
+	// n 및 최대 스태미나 설정
+	n = N;
+	max_stamina = mMaxStamina;
+
+	// 맵 정보 초기화
+	gate_map.clear();
+	add_cnt = 0;
+	call_cnt = 0;
+
+	for (int i = 0; i < 202; ++i)
+		gate_graph[i].clear();
+
+	memset(gate_edge, 0, sizeof(gate_edge));
+	memset(visit, 0, sizeof(visit));
+	memset(dijk_visit, 0, sizeof(dijk_visit));
+
+	// 맴 정보 복사
+	for (int i = 0; i < N; ++i)
+		map[i] = mMap[i];
+
+	return;
+}
+
+
+/* 게이트 추가 함수 */
+void addGate(int mGateID, int mRow, int mCol)
+{
+	// 벽이 1이기 때문에 게이트 번호를 2부터 시작
+	mGateID += 1;
+
+	// 맵에 게이트 추가
+	map[mRow][mCol] = mGateID;
+	gate_map[mGateID] = mRow * n + mCol;
+	add_gate[add_cnt++] = mGateID;
+
+	return;
+}
+
+
+/* 게이트 제거 함수 */
+void removeGate(int mGateID)
+{
+	// 벽이 1이기 때문에 게이트 번호를 2부터 시작
+	mGateID += 1;
+
+	// 맵에서 게이트 제거
+	int cur = gate_map[mGateID];
+	map[cur / n][cur % n] = 0;
+	gate_map[mGateID] = 0;
+
+	return;
+}
+
+
+
+
+/* 생성된 게이트 간 최소 이동 시간 계산 */
+void bfs() {
+	for (int i = 0; i < add_cnt; i++) {
+		int gate_id = add_gate[i];
+		//cout << "add gate id: " << gate_id << "\n";
+		// 추가된 게이트가 활성화 되어 있다면
+		if (gate_map[gate_id]) {
+			//cout << "this active gate\n";
+			// 해당 게이트의 좌표를 가져온다
+			int y = gate_map[gate_id] / n;
+			int x = gate_map[gate_id] % n;
+
+			// 큐 초기화 및 방문 기록 초기화
+			front = rear = 0;
+			que[rear++] = { y, x, 0 };
+			visit[y][x] = { gate_id, 0 };
+
+			// BFS 알고리즘 수행
+			while (front < rear) {
+				BFS_INFO cur = que[front++];
+				//cout << "cur: " << cur.y << ", " << cur.x << ", " << cur.time << "\n";
+				//cout << "visit map: " << map[cur.y][cur.x] << "\n";
+				// 최대 이동 가능 거리를 넘었다면 continue
+				if (cur.time > max_stamina) {
+					continue;
+				}
+
+				// 다른 게이트를 만났다면, 최소 이동 시간 기록 및 continue
+				if (map[cur.y][cur.x] > 1 && map[cur.y][cur.x] != gate_id) {
+					visit[cur.y][cur.x] = { gate_id, cur.time };
+					if (!gate_edge[gate_id][map[cur.y][cur.x]]) {
+						gate_edge[gate_id][map[cur.y][cur.x]] = true;
+						gate_edge[map[cur.y][cur.x]][gate_id] = true;
+
+						gate_graph[gate_id].push_back({ map[cur.y][cur.x], cur.time });
+						gate_graph[map[cur.y][cur.x]].push_back({ gate_id, cur.time });
+					}
+
+					//cout << "now graph status\n";
+					//for (int i = 0; i < 202; i++) {
+					//	if (gate_map[i]) {
+					//		cout << "gate_id: " << i << "\n";
+					//		for (NODE_INFO next : gate_graph[i]) {
+					//			cout << "(" << next.gate_id << ", " << next.time << ") ";
+					//		}
+					//		cout << "\n";
+					//	}
+					//}
+				}
+
+				// 4방향 탐색
+				for (int d = 0; d < 4; d++) {
+					int ny = cur.y + dy[d];
+					int nx = cur.x + dx[d];
+
+					// 방문한 적 없고, 벽이 아니라면 방문 기록 및 큐에 추가
+					if (visit[ny][nx].gate_id != gate_id && map[ny][nx] != 1) {
+						visit[ny][nx] = { gate_id, cur.time + 1 };
+						que[rear++] = { ny, nx, cur.time + 1 };
+					}
+					// 방문한 적이 있고, 현재 시간보다 더 빠른 시간으로 도착했다면 방문 기록 갱신 및 큐에 추가
+					else if (visit[ny][nx].gate_id == gate_id) {
+						if (visit[ny][nx].time > cur.time + 1) {
+							visit[ny][nx].time = cur.time + 1;
+							que[rear++] = { ny, nx, cur.time + 1 };
+						}
+					}
+				}
+			}
+		}
+	}
+	add_cnt = 0;
+}
+
+
+/* 최소 이동 시간 계산 함수 */
+int getMinTime(int mStartGateID, int mEndGateID)
+{
+	call_cnt++;
+	// 생성된 게이트 간 최소 이동 시간 계산
+	bfs();
+	//cout << "now graph status\n";
+	//for (int i = 0; i < 202; i++) {
+	//	if (gate_map[i]) {
+	//		cout << "gate_id: " << i - 1<< "\n";
+	//		for (NODE_INFO next : gate_graph[i]) {
+	//			cout << "(" << next.gate_id - 1 << ", " << next.time << ") ";
+	//		}
+	//		cout << "\n";
+	//	}
+	//}
+
+	// 벽이 1이기 때문에 게이트 번호를 2부터 시작
+	mStartGateID += 1;
+	mEndGateID += 1;
+
+	//cout << "start gate id: " << mStartGateID - 1 << ", end gate id: " << mEndGateID - 1 << "\n";
+	priority_queue<NODE_INFO, vector<NODE_INFO>, Comp> pq;
+	dijk_visit[mStartGateID] = { call_cnt, 0 };
+	pq.push({ mStartGateID, 0 });
+	while (!pq.empty()) {
+		NODE_INFO cur = pq.top();
+		pq.pop();
+
+		//cout << "now gate id: " << cur.gate_id - 1 << ", time: " << cur.time << "\n";
+
+		// 목적지점에 도착했다면 최소 이동 시간 반환
+		if (cur.gate_id == mEndGateID) {
+			return cur.time;
+		}
+
+		// 현재 게이트에서 연결된 게이트들을 탐색
+		for (NODE_INFO next : gate_graph[cur.gate_id]) {
+			//cout << "next gate id: " << next.gate_id - 1 << ", status: " << gate_map[next.gate_id] << "\n";
+			// 활성화된 게이트라면
+			if (gate_map[next.gate_id]) {
+				// 현재 게이트에서 연결된 게이트로 이동했을 때, 최소 이동 시간 갱신 및 우선순위 큐에 추가
+				if (dijk_visit[next.gate_id].call_cnt < call_cnt) {
+					dijk_visit[next.gate_id] = { call_cnt, cur.time + next.time };
+					pq.push({ next.gate_id, cur.time + next.time });
+				}
+				else if (dijk_visit[next.gate_id].time > cur.time + next.time) {
+					dijk_visit[next.gate_id].time = cur.time + next.time;
+					pq.push({ next.gate_id, cur.time + next.time });
+				}
+			}
+		}
+	}
+
+	//cout << "out of que\n";
+	return -1;
+}
+
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+
+
+#define MAX_MAP_SIZE 350
+
+/////////////////////////////////////////////////////////////////////////
+
+#define CMD_INIT			0
+#define CMD_ADD_GATE		1
+#define CMD_REMOVE_GATE		2
+#define CMD_GET_MIN_TIME	3
+
+static int gMap[MAX_MAP_SIZE][MAX_MAP_SIZE];
+
+static bool run()
+{
+	int cmd, ans, ret;
+	int N, maxStamina, gateID1, gateID2, row, col;
+	int Q = 0;
+	bool okay = false;
+
+	cin >> Q;
+
+	for (int q = 0; q < Q; ++q)
+	{
+		cin >> cmd;
+
+		switch (cmd)
+		{
+		case CMD_INIT:
+			cin >> N >> maxStamina;
+			for (int i = 0; i <= N - 1; i++) {
+				for (int j = 0; j <= N - 1; j++) {
+					cin >> gMap[i][j];
+				}
+			}
+			init(N, maxStamina, gMap);
+			okay = true;
+			break;
+
+		case CMD_ADD_GATE:
+			cin >> gateID1 >> row >> col;
+			addGate(gateID1, row, col);
+			break;
+
+		case CMD_REMOVE_GATE:
+			cin >> gateID1;
+			removeGate(gateID1);
+			break;
+
+		case CMD_GET_MIN_TIME:
+			cin >> gateID1 >> gateID2;
+			ret = getMinTime(gateID1, gateID2);
+			cin >> ans;
+			//cout << "-------------------------------------------------\n";
+			//cout << "ret: " << ret << ", ans: " << ans << "\n";
+			if (ret != ans) {
+				//cout << "fail\n";
+				okay = false;
+			}
+			break;
+
+		default:
+			okay = false;
+		}
+	}
+
+	return okay;
+}
+
+int main()
+{
+	//freopen("sample_input.txt", "r", stdin);
+
+	int T, MARK;
+	cin >> T >> MARK;
+
+	for (int tc = 1; tc <= T; tc++)
+	{
+		int score = run() ? MARK : 0;
+		cout << "#" << tc << " " << score << endl;
+	}
+
+	return 0;
+}
